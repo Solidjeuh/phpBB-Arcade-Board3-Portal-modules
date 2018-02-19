7e5d2b443f11665664c09aa14ac1c3c3ab9bb9c2
Andy Dm <demarieandy@outlook.com>
2018-02-19 17:00:12 +0100

    First upload

---
+++ jv/arcade_b3portal/language/nl/acp_settings.php
@@ -0,0 +1,46 @@
+<?php
+/**
+*
+* @package phpBB Arcade - Board3 portal arcade modules
+* @version $Id$
+* @author 2011-2018 KillBill - killbill@jatek-vilag.com
+* @copyright (c) 2014-2018 https://jv-arcade.com/ - support@jv-arcade.com
+* Nederlandse vertaling @ Solidjeuh <https://www.muziekpromo.net>
+* @license https://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+// Some characters you may want to copy&paste: ‚ ‘ ’ « » „ “ ” …
+
+//These are used through out the arcade
+$lang = array_merge($lang, array(
+	'ARCADE_EXT_B3PORTAL'						=> 'Board3 portaal arcade modules',
+	'ARCADE_EXT_B3PORTAL_DETECT'				=> 'De “Board3 Portaal” extensie is niet ge&iuml;nstalleerd of uitgeschakeld, dus deze extensie is inactief.',
+	'ARCADE_EXT_B3PORTAL_EXPLAIN'				=> 'Alle ge&iuml;nstalleerde arcade portaal-modules worden weergegeven op het portaal.',
+	'ARCADE_EXT_B3PORTAL_STATS_AUTH'			=> 'Statistieken permissie controle',
+	'ARCADE_EXT_B3PORTAL_STATS_AUTH_EXPLAIN'	=> 'Indien u de Statistieken permissie controle uitschakelt kunnen diegene die geen permissie hebben om de arcade statistieken te bekijken de arcade portaal modules wel bekijken.',
+));
---
+++ jv/arcade_b3portal/language/nl/modules.php
@@ -0,0 +1,68 @@
+<?php
+/**
+*
+* @package phpBB Arcade - Board3 portal arcade modules
+* @version $Id$
+* @author 2011-2018 KillBill - killbill@jatek-vilag.com
+* @copyright (c) 2014-2018 https://jv-arcade.com/ - support@jv-arcade.com
+* Nederlandse vertaling @ Solidjeuh <https://www.muziekpromo.net>
+* @license https://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+// Some characters you may want to copy&paste: ‚ ‘ ’ « » „ “ ” …
+
+//These are used through out the arcade
+$lang = array_merge($lang, array(
+	'ARCADE_EXT_B3PORTAL_BEST_RATED_GAMES_TITLE'				=> 'Arcade - Best beoordeelde spellen',
+	'ARCADE_EXT_B3PORTAL_CHALLENGE_LEADERS'						=> 'Arcade uitdaging - leiders',
+	'ARCADE_EXT_B3PORTAL_CHALLENGE_LEAST_PLAYED_USERS_TITLE'	=> 'Arcade uitdaging - Minst gespeelde gebruikers',
+	'ARCADE_EXT_B3PORTAL_CHALLENGE_LEAST_POPULAR_GAMES_TITLE'	=> 'Arcade uitdaging - Minst populaire games',
+	'ARCADE_EXT_B3PORTAL_CHALLENGE_MOST_PLAYED_USERS_TITLE'		=> 'Arcade uitdaging - Meest gespeelde gebruikers',
+	'ARCADE_EXT_B3PORTAL_CHALLENGE_MOST_POPULAR_GAMES_TITLE'	=> 'Arcade uitdaging - Meest populaire spellen',
+	'ARCADE_EXT_B3PORTAL_DISPLAYED_DATA'						=> 'Weergegeven gegevens',
+	'ARCADE_EXT_B3PORTAL_HIGH_JACKPOT_TITLE'					=> 'Arcade - Hoge jackpot',
+	'ARCADE_EXT_B3PORTAL_LATEST_HIGHSCORES'						=> 'Arcade - Laatste highscores',
+	'ARCADE_EXT_B3PORTAL_LEADERS'								=> 'Arcade leiders',
+	'ARCADE_EXT_B3PORTAL_LEAST_PLAYED_USERS_TITLE'				=> 'Arcade - Minst gespeelde gebruikers',
+	'ARCADE_EXT_B3PORTAL_LEAST_POPULAR_GAMES_TITLE'				=> 'Arcade - Minst populaire spellen',
+	'ARCADE_EXT_B3PORTAL_LONGEST_HELD_SCORES'					=> 'Arcade - Langste behouden highscores',
+	'ARCADE_EXT_B3PORTAL_LONGEST_PLAYTIME_TITLE'				=> 'Arcade - Langste speeltijd',
+	'ARCADE_EXT_B3PORTAL_LOW_JACKPOT_TITLE'						=> 'Arcade - lage jackpot',
+	'ARCADE_EXT_B3PORTAL_MOST_PLAYED_USERS_TITLE'				=> 'Arcade - Meest gespeelde gebruikers',
+	'ARCADE_EXT_B3PORTAL_MOST_POPULAR_GAMES_TITLE'				=> 'Arcade - Meest populaire spellen',
+	'ARCADE_EXT_B3PORTAL_NEWEST_GAMES_TITLE'					=> 'Arcade - Nieuwste spellen',
+	'ARCADE_EXT_B3PORTAL_ONLINE'								=> 'Wie speelt er in de arcade?',
+	'ARCADE_EXT_B3PORTAL_RANDOM_GAMES_TITLE'					=> 'Arcade - willekeurige spellen',
+	'ARCADE_EXT_B3PORTAL_SHORTEST_PLAYTIME_TITLE'				=> 'Arcade - Kortste speeltijd',
+	'ARCADE_EXT_B3PORTAL_SUPER_CHAMPIONS_TITLE'					=> 'Arcade - Arcade super kampioenen',
+	'ARCADE_EXT_B3PORTAL_TOURNAMENT_LEADERS'					=> 'Arcade toernooi leiders',
+	'ARCADE_EXT_B3PORTAL_TOUR_LEAST_PLAYED_USERS_TITLE'			=> 'Arcade toernooi - Minst gespeelde gebruikers',
+	'ARCADE_EXT_B3PORTAL_TOUR_LEAST_POPULAR_GAMES_TITLE'		=> 'Arcade toernooi - Minst populaire spellen',
+	'ARCADE_EXT_B3PORTAL_TOUR_MOST_PLAYED_USERS_TITLE'			=> 'Arcade toernooi - Meest gespeelde gebruikers',
+	'ARCADE_EXT_B3PORTAL_TOUR_MOST_POPULAR_GAMES_TITLE'			=> 'Arcade toernooi - Meest populaire spellen',
+));
